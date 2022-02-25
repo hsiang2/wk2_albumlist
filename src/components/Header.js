f40a51499b29f26ec4750b1425a3da6c74c42a24
Hsiang <q224399377@gmail.com>
2022-02-26 00:49:23 +0800

    add header

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Header = () => {
+    return(
+        <View style = {styles.headerStyle}>
+            <Text style = {styles.textStyle}>Albums</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    headerStyle: {
+
+    },
+    textStyle: {
+
+    },
+});
+
+export default Header;
