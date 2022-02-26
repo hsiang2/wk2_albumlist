ae8f4718bf6f384742aa9e77d265e73bdd3a5951
Hsiang <q224399377@gmail.com>
2022-02-26 17:17:09 +0800

    add album list

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,3 +1,18 @@
 import React from "react";
 import { ScrollView } from "react-native";
 import AlbumDetail from "./AlbumDetail";
+import albumData from "../json/albums.json"
+
+const AlbumList = () => {
+    return(
+        <ScrollView>
+            <AlbumDetail album = {albumData[0]} />
+            <AlbumDetail album = {albumData[1]} />
+            <AlbumDetail album = {albumData[2]} />
+            <AlbumDetail album = {albumData[3]} />
+            <AlbumDetail album = {albumData[4]} />
+        </ScrollView>
+    );
+};
+
+export default AlbumList;
